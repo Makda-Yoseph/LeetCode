f5796bcf99a2def1344f80f7def3be3f9c7c82ca
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2023-12-16 23:37:12 +0300

    Added solution for Frequency Tracker

---
+++ week1/frequency-tracker.py
@@ -0,0 +1,28 @@
+class FrequencyTracker:
+    
+    
+    def __init__(self):
+        self.occur = defaultdict(int)
+        self.freq = defaultdict(int)
+
+    def add(self, number: int) -> None:
+        self.occur[number] +=1
+        f = self.occur[number]
+        self.freq[f] += 1
+        self.freq[f-1] -= 1
+    def deleteOne(self, number: int) -> None:
+        if self.occur[number] > 0:
+            self.occur[number] -=1
+            f = self.occur[number]
+            self.freq[f] +=1
+            self.freq[f+1] -=1
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq[frequency]>0
+        
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
