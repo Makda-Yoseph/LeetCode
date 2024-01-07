7b8404b309c560a692f6954a552d19955631f30e
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2024-01-07 21:44:43 +0300

    Added solution for Sort Colors

--- week1/sort-colors.py
+++ week1/sort-colors.py
@@ -1,8 +1,12 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         for i in range(len(nums)-1):
             for j in range(len(nums)-i-1):
                 if nums[j]> nums[j+1]:
                     nums[j],nums[j+1]= nums[j+1],nums[j]
-        return nums
+        
+        
         
