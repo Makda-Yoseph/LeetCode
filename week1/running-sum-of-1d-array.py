7677a6a18eb302db03b8f3084ec31f8b2c3a2abb
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2024-01-16 17:43:09 +0300

    Added solution for Running Sum of 1d Array

--- week1/running-sum-of-1d-array.py
+++ week1/running-sum-of-1d-array.py
@@ -1,7 +1,10 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        if nums is None or len(nums) == 0:
-            return nums
-        for i in range(1, len(nums)):
-            nums[i] += nums[i-1]
+        # if nums is None or len(nums) == 0:
+        #     return nums
+        # for i in range(1, len(nums)):
+        #     nums[i] += nums[i-1]
+        # return nums
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1] 
         return nums
