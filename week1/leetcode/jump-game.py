c58e8c8c45a03ce2686af7f501087907fbd63e8b
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2024-02-22 09:31:20 +0300

    Add solution for Jump Game

---
+++ week1/leetcode/jump-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        step = 0
+        for i in range(len(nums)-1):
+            step = max(step,i + nums[i])
+            if step == i:
+                return False
+        return True
+            
+
+        
